e946ecdb98483c7978f9f50f2faa287095af0977
Cheer Chokwittaya <cheer.chokwit@gmail.com>
2024-08-27 11:17:08 -0500

    change provider to node

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const serverless = require('serverless-http');
+const express = require('express');
+
+const app = express();
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/health', (_, res) => {
+  return res.status(200).json({
+    message: 'This is the response from AWS lambda',
+  });
+});
+
+app.use((_, res) => {
+  return res.status(404).json({
+    error: 'Not Found',
+  });
+});
+
+exports.handler = serverless(app);
+app.listen(3000, () => {
+  console.log('app running');
+});
